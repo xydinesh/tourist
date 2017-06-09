3ab1d4e82727a3fb3c5e9c92465dfa7b114d2111
Dinesh Weerapurage <dinesh.weerapurage@pearson.com>
2017-06-09 06:48:26 -0600

    Loading TSP files for calculation

--- pkg/tourist.go
+++ pkg/tourist.go
@@ -1,8 +1,12 @@
 package pkg
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
+	log "github.com/sirupsen/logrus"
+	"os"
+	"strconv"
+	"strings"
 )
 
 type Node struct {
@@ -20,11 +24,88 @@ type TSPInstance struct {
 	Nodes          []Node
 }
 
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	// log.SetFormatter(&log.JSONFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.DebugLevel)
+}
+
 func ReadDataFile(filename string) (TSPInstance, error) {
-	b, err := ioutil.ReadFile(filename)
+	log.WithFields(log.Fields{"filename": filename}).Debug("Reading file for data")
+
+	f, err := os.Open(filename)
 	if err != nil {
 		return TSPInstance{}, err
 	}
-	fmt.Printf("%v", b)
-	return TSPInstance{}, nil
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+	parserState := 0
+	tsp := TSPInstance{}
+	for s.Scan() {
+		switch parserState {
+		case 0:
+			{
+				data := strings.Split(s.Text(), ":")
+				if len(data) > 1 {
+					key := strings.TrimSpace(data[0])
+					value := strings.TrimSpace(data[1])
+					switch key {
+					case "NAME":
+						tsp.Name = value
+						log.WithField(key, value).Debug("Name")
+					case "COMMENT":
+						tsp.Comments = append(tsp.Comments, value)
+						log.WithField(key, value).Debug("Comment")
+					case "TYPE":
+						tsp.Type = value
+						log.WithField(key, value).Debug("Type")
+					case "DIMENSION":
+						d, err := strconv.Atoi(value)
+						if err != nil {
+							log.WithError(err).Error("String to Int conversion failed")
+						}
+						tsp.Dimension = d
+						log.WithField(key, value).Debug("Dimension")
+					case "EDGE_WEIGHT_TYPE":
+						tsp.EdgeWeightType = value
+						log.WithField(key, value).Debug("EdgeWeightType")
+					default:
+						log.WithField(key, value).Warning("Didn't find any matching fielts")
+					}
+				} else {
+					parserState = 1
+				}
+			}
+		case 1:
+			{
+				data := strings.Split(s.Text(), " ")
+				if len(data) >= 3 {
+					node := Node{}
+
+					node.Id = data[0]
+					f, err := strconv.ParseFloat(data[1], 64)
+					if err != nil {
+						log.WithError(err).Error("Float parse failed")
+					}
+					node.X = float32(f)
+
+					f, err = strconv.ParseFloat(data[2], 64)
+					if err != nil {
+						log.WithError(err).Error("Float parse failed")
+					}
+					node.Y = float32(f)
+					tsp.Nodes = append(tsp.Nodes, node)
+				}
+			}
+		}
+	}
+
+	return tsp, nil
 }
--- pkg/tourist_pkg_test.go
+++ pkg/tourist_pkg_test.go
@@ -1,11 +1,13 @@
 package pkg
 
 import (
+	log "github.com/sirupsen/logrus"
 	"testing"
 )
 
 func TestNodeStruct(t *testing.T) {
 	t.Log("Testing Node structure")
+
 	node := Node{}
 	if id := node.Id; id != "" {
 		t.Errorf("Expected id is null, got %s \n", id)
@@ -30,5 +32,40 @@ func TestReadDataFile(t *testing.T) {
 
 	if d := tsp_instance.Dimension; d != 29 {
 		t.Errorf("Expected 29, got %d", d)
+		log.WithFields(log.Fields{"expected": 29,
+			"result": d}).Error("Expected value does not match with return value")
 	}
+
+	if tsp_instance.Name != "wi29" {
+		log.WithField("Name", tsp_instance.Name).Error("Return value is different from expected value of wi29")
+	}
+
+	if tsp_instance.Type != "TSP" {
+		log.WithField("TSP", tsp_instance.Name).Error("Return value is different from expected value of TSP")
+	}
+
+	if tsp_instance.EdgeWeightType != "EUC_2D" {
+		log.WithField("Edge weight type", tsp_instance.Name).Error("Return value is different from expected value of EUC_2D")
+	}
+
+	if len(tsp_instance.Nodes) != tsp_instance.Dimension {
+		log.WithFields(log.Fields{"NodeCount": len(tsp_instance.Nodes),
+			"Dimension": tsp_instance.Dimension}).Error("Node count is not equal to dimension of the instance")
+	}
+
+	node := tsp_instance.Nodes[0]
+	if node.Id != "1" || node.X != 20833.3333 || node.Y != 17100.0000 {
+		log.WithFields(log.Fields{"Id": node.Id, "X": node.X, "Y": node.Y}).Error("Node does not have expected values")
+	}
+
+	node = tsp_instance.Nodes[28]
+	if node.Id != "29" || node.X != 27462.5000 || node.Y != 12992.2222 {
+		log.WithFields(log.Fields{"Id": node.Id, "X": node.X, "Y": node.Y}).Error("Node does not have expected values")
+	}
+
+	node = tsp_instance.Nodes[10]
+	if node.Id != "11" || node.X != 23700.0000 || node.Y != 15933.3333 {
+		log.WithFields(log.Fields{"Id": node.Id, "X": node.X, "Y": node.Y}).Error("Node does not have expected values")
+	}
+
 }
