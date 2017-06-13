4fa9b99409632ee5cee026cb3a8d8e87a7ec247a
Dinesh Weerapurage <xydinesh@gmail.com>
2017-06-13 06:14:10 -0600

    Added command line options

--- cmd/solve.go
+++ cmd/solve.go
@@ -15,11 +15,17 @@ package cmd
 
 import (
 	"fmt"
-
+  tourist "github.com/xydinesh/tourist/pkg"
 	"github.com/spf13/cobra"
 )
 
 var TSPFile string
+var Temperature float64
+var Beta float64
+var Iterations int
+var Goal float32
+var Output int
+var Solution bool
 
 // solveCmd represents the solve command
 var solveCmd = &cobra.Command{
@@ -27,12 +33,28 @@ var solveCmd = &cobra.Command{
 	Short: "Solve TSP for given instance",
 	Long:  `Solve Traveling Salesman Problem (TSP) for a given problem instance provided as a text file.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("solve called")
-		fmt.Printf("%s\n", TSPFile)
+		tsp_instance, err := tourist.ReadDataFile(TSPFile)
+		if err != nil {
+			fmt.Errorf("An error happened, %s", err)
+		}
+		r := tourist.GenerateRandomRoute(tsp_instance.Dimension)
+		s := tourist.StopConditon{Goal: Goal, Iterations: Iterations, Output: Output}
+		nr := tsp_instance.ComputeOptimalRoute(&r, Temperature, Beta, &s)
+		cost := tsp_instance.GetRouteCost(&nr)
+		fmt.Printf("Final Cost: %f\n", cost)
+		if Solution == true {
+			fmt.Printf("Final Solution: %v\n", nr.NodeOrder)
+		}
 	},
 }
 
 func init() {
-	solveCmd.Flags().StringVarP(&TSPFile, "tsp", "t", "", "File with TSP instance")
+	solveCmd.Flags().StringVarP(&TSPFile, "file", "f", "", "File with TSP instance")
+	solveCmd.Flags().Float64VarP(&Temperature, "temp", "t", 10.0, "Initial Temperature for the instance")
+	solveCmd.Flags().Float64VarP(&Beta, "beta", "b", 0.99981, "Beta value for reducing temperature")
+	solveCmd.Flags().Float32VarP(&Goal, "goal", "g", 0.0, "Cost value of the solution")
+	solveCmd.Flags().IntVarP(&Iterations, "iterations", "i", 1000, "Iterations to run")
+	solveCmd.Flags().IntVarP(&Output, "output", "o", 1000, "How often to see an output")
+	solveCmd.Flags().BoolVarP(&Solution, "solution", "s", false, "Print final solution")
 	RootCmd.AddCommand(solveCmd)
 }
